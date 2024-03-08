b9c768d9e0b14a20877de2170e53aa9e1a68b052
Pantelis Flouris <68327978+paflou@users.noreply.github.com>
2024-03-08 20:05:42 +0200

    .

--- organizer.py
+++ organizer.py
@@ -10,31 +10,35 @@ def organize():
         if file.endswith(".txt") or file.endswith(".pdf") or file.endswith(".docx") \
         or file.endswith(".doc") or file.endswith(".odt") or file.endswith(".xls") \
         or file.endswith(".xlsx") or file.endswith(".ppt") or file.endswith(".pptx"):
-            print("Moving "+ file + " to document folder")
             documents(file)
 
         #audio file formats
         elif file.endswith(".m4a") or file.endswith(".mp3") or file.endswith(".flac") \
         or file.endswith(".wav") or file.endswith(".wma") or file.endswith(".aac"):
-            print("Moving " + file +" to audio folder")
             audio(file)
 
         #video file formats
         elif file.endswith(".mp4") or file.endswith(".mov") or file.endswith(".avi") \
         or file.endswith(".wmv") or file.endswith(".webm"):
-            print("Moving "+ file + " to videos folder")
             videos(file)
 
         #image file formats
-        elif file.endswith(".jpeg") or file.endswith(".gif") or file.endswith(".png") \
+        elif file.endswith(".jpg") or file.endswith(".gif") or file.endswith(".png") \
         or file.endswith(".tiff") or file.endswith(".bmp") or file.endswith(".psd") \
         or file.endswith(".raw") or file.endswith(".heif"):
-            print("Moving "+ file + " to images folder")
             images(file)
+
+        #archive file formats
+        elif file.endswith(".zip") or file.endswith(".7z") or file.endswith(".rar") \
+        or ".tar" in file:
+            archives(file)
+
+        elif "." not in file:
+            folders(file)
 
         #everything else
         else:
-            if(file == "organizer.py"):
+            if file == "organizer.py" or  "." not in file:
                 continue
             print("Moving "+ file + " to misc folder")
             misc(file)
@@ -43,22 +47,42 @@ def organize():
 def documents(file):
     if(not os.path.exists(os.getcwd() + "/documents")):
         os.mkdir("documents")
+    print("Moving "+ file + " to document folder")
     shutil.move(file,"documents/" + file)
 
 def audio(file):
     if(not os.path.exists(os.getcwd() + "/audio")):
         os.mkdir("audio")
+    print("Moving " + file +" to audio folder")
     shutil.move(file,"audio/" + file)
 
 def videos(file):
     if(not os.path.exists(os.getcwd() + "/videos")):
         os.mkdir("videos")
+    print("Moving "+ file + " to videos folder")
     shutil.move(file,"videos/" + file)
 
 def images(file):
     if(not os.path.exists(os.getcwd() + "/images")):
         os.mkdir("images")
+    print("Moving "+ file + " to images folder")
     shutil.move(file,"images/" + file)
+
+def archives(file):
+    if(not os.path.exists(os.getcwd() + "/archives")):
+        os.mkdir("archives")
+    print("Moving "+ file + " to archives folder")
+    shutil.move(file,"archives/" + file)
+
+def folders(file):
+    if(not os.path.exists(os.getcwd() + "/folders")):
+        os.mkdir("folders")
+        
+    #dont put your folders
+    if not(file == "documents" or file == "audio" or file == "videos" \
+    or file == "images" or file == "archives" or file =="misc" or file == "folders"):
+        print("Moving "+ file + " to folders folder")
+        shutil.move(file,"folders/" + file)
 
 def misc(file):
     if(not os.path.exists(os.getcwd() + "/misc")):
